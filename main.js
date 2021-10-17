5b8c45cd694af3bcf60281af5bc52f5415b8dcfc
Carly Armour <carlyarmour@gmail.com>
2021-10-17 13:25:07 -0500

    correct errors

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ if (name !== 'null') {
     alert("Welcome, " + name + "! Contact us if you need anything.");
 }
 
+var greeting = document.getElementById("greeting");
+var day = new Date();
+var hr = day.getHours();
+if (hr >= 0 && hr < 12) {
+  greeting.textContent = ("Good Morning!");
+} else if (hr == 12) {
+  greeting.textContent = ("Good Noon!");
+} else if (hr >= 12 && hr <= 17) {
+  greeting.textContent = ("Good Afternoon!");
+} else {
+  greeting.textContent = ("Good Evening!");
+}

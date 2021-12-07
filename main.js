8bdf0e74ce036a6a6953959f57c1cb00c66279a1
Carly Armour <carlyarmour@gmail.com>
2021-12-06 19:54:02 -0600

    add coffee function

--- main.js
+++ main.js
@@ -13,8 +13,25 @@ if (hr >= 0 && hr < 12) {
 
 // program to display text 5 times
 const n = 5;
-
 // looping from i = 1 to 5
 for (let i = 1; i <= n; i++) {
     console.log(`I love java/tea! Or give chocolate!`);
 }
+
+function myFunction() {
+  var javatea = document.getElementById("question").value;
+  var parent = document.getElementById('image');
+  var imagePath = "fullcoffee.jpg";
+
+  for (let i = javatea; i > 0; i--) {
+      var img = new Image();
+      img.src = imagePath;
+      parent.appendChild(img);
+  }
+}
+
+var coffeeForm = document.getElementById("Coffee");
+coffeeForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  secondFunction();
+});
